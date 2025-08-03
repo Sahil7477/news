8af85ed164178a933fc2600ff76ae34b3f3e33d5
Sahil <sahilaktar648@gmail.com>
2025-08-03 17:25:14 +0530

    minor changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,10 +9,10 @@ const Footer = () => {
           <div>
             <div className="mb-4">
               <div className="text-xl font-bold">
-                <span className="font-english text-white">Hindustan</span>
-                <span className="font-english text-white">Times</span>
+                <span className="font-english text-white">Sangbad</span>
+                
               </div>
-              <div className="text-2xl font-bold text-news-red font-bengali">বাংলা</div>
+              <div className="text-2xl font-bold text-news-red font-bengali">Bangla</div>
             </div>
             <p className="text-sm font-bengali">
               পশ্চিমবঙ্গের শীর্ষ অনলাইন বাংলা সংবাদপত্র
@@ -47,8 +47,8 @@ const Footer = () => {
           <div>
             <h3 className="font-semibold mb-4 font-bengali">যোগাযোগ</h3>
             <ul className="space-y-2 text-sm">
-              <li className="font-english">Email: bangla@hindustantimes.com</li>
-              <li className="font-english">Phone: +91 98765 43210</li>
+              <li className="font-english">Email: xyzexample@gmail.com</li>
+              <li className="font-english">Phone: +91 1234567890</li>
               <li className="font-bengali">ঠিকানা: কলকাতা, পশ্চিমবঙ্গ</li>
             </ul>
           </div>
@@ -56,7 +56,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-700 mt-8 pt-8 text-center">
           <p className="text-sm font-english">
-            © 2025 Hindustan Times Bangla. All rights reserved.
+            © 2025 Sangbad Bangla. All rights reserved.
           </p>
         </div>
       </div>
